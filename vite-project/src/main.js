7cf9b58fda0e86917b91a09f63665675bfe683fc
giangltph33114 <giangltph33114@fpt.edu.vn>
2024-01-12 12:13:28 +0700

    sd cmit

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,1 +1,9 @@
-console.log("1");
+
+import classes from "./main.module.css";
+document.querySelector('#app').innerHTML = 
+`
+    <div class="${classes.red}">
+        hello world
+    </div>
+`
+;
